12a4a8817cecadcfb10be1f6a0995a063410cef3
akeshavan <keshavan@berkeley.edu>
2018-05-10 16:26:40 +0100

    added firebase keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 // copy/paste this from your firebase console
-const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
+var config = {
+  apiKey: "AIzaSyD08blKQnxP3FR1hk6aMDv6YM4mdV8XwoQ",
+  authDomain: "appstract0.firebaseapp.com",
+  databaseURL: "https://appstract0.firebaseio.com",
+  projectId: "appstract0",
+  storageBucket: "appstract0.appspot.com",
+  messagingSenderId: "435321691621"
 };
 
 firebase.initializeApp(config);
